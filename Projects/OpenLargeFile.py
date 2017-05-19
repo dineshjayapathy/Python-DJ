d78d9822d793b221013c35239415db29a94c4e0a
dineshjayapathy <dinesh.jayapathy@arcadiasolutions.com>
2017-05-19 12:43:44 -0400

    writes the sample to the output

--- Projects/OpenLargeFile.py
+++ Projects/OpenLargeFile.py
@@ -1,17 +1,18 @@
 import os
 import subprocess
-def realargefile(fpath):
-
+#from __future__ import print_function
+def readlargefile(fpath):
     s = raw_input('Enter the path of files:\n')
     for i in os.listdir(s):
         print i
-        if os.path.isfile(os.path.join(s, i)) and i.endswith('MEDICAL.txt'):
-
+        if os.path.isfile(os.path.join(s, i)) and i.endswith('.txt'):
+            f = open('C:\DJ\Bidco UI\OPTUM\data\Sample Athena -Optum feed\\' + i , 'w')
             dr = (os.path.join(s, i))  # works
             dr = '/'.join(dr.split('\\'))
 
             with open(dr) as infile:
-                head = [next(infile) for x in xrange(100)]
-                #print str(head)
+                head = [next(infile) for x in xrange(1000)]
                 print "\n".join(head)
-realargefile('a')
+                f.write("\n".join(head))
+            f.close()
+readlargefile('a')
