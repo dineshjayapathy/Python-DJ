d55b1a94a7a9104e7810d9f7926e5845a9100350
dineshjayapathy <dinesh.jayapathy@arcadiasolutions.com>
2017-08-09 16:04:22 -0400

    Ading Due date, PID as Customer Contract ID, formatting description

--- Projects/Jira-BulkUpload_CR.py
+++ Projects/Jira-BulkUpload_CR.py
@@ -62,7 +62,11 @@ sys.setdefaultencoding('utf-8')
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=QDWSQLOPS01;DATABASE=AnalyticsMonitoring;Trusted_Connection=TRUE')
 cursor = cnxn.cursor()
 
-cursor.execute("select Client_Acronym, Data_Source_Acronym, CR_Name, cr_Important_Context, CR_Owner_Email_Address, TRY_CONVERT(date,insert_timestamp), ID, CR_WorkEffortEstimate from ARC_OrderFormValues where JIRA_Ticket is NULL and CR_Name is not null")
+#This is the valid one
+cursor.execute("select Client_Acronym, Data_Source_Acronym, CR_Name, cr_Important_Context, CR_Owner_Email_Address, TRY_CONVERT(date,insert_timestamp), ID, CR_WorkEffortEstimate,CR_date_due,CR_Finance_team_PID,CR_Scope from ARC_OrderFormValues where JIRA_Ticket is NULL and CR_Name is not null")
+
+#test sql
+# cursor.execute("select Client_Acronym, Data_Source_Acronym, CR_Name, cr_Important_Context, CR_Owner_Email_Address, TRY_CONVERT(date,insert_timestamp), ID, CR_WorkEffortEstimate,CR_date_due,CR_Finance_team_PID,CR_Scope from ARC_OrderFormValues where ID='268'")
 
 
 
@@ -85,10 +89,17 @@ else:
         context=i[3]
         id1 = str(i[6])
         estimate = str(i[7])+'h'
-        # watcher='dinesh.jayapathy@arcadiasolutions.com'
+        watcher='dinesh.jayapathy'
         # watcher=watcher.replace('@arcadiasolutions.com','')
-        watcher=i[4].replace('@arcadiasolutions.com','')
+        # watcher=i[4].replace('@arcadiasolutions.com','')
         #print 'Here are the variables we need',count, client, source,order_name
+        duedate=str(i[8])
+        duedate=duedate[:10]
+        finid=i[9]
+        scope=i[10]
+
+        if finid is None:
+            finid='Unknown'
 
         issue_d = {
 
@@ -102,18 +113,23 @@ else:
             # 'customfield_11601': {'id': '12196' }, #this is client. This is causing an error for MODA. Not sure why.
             'customfield_11609': {'value': source },#this is the data source. It should be an existing value. Else it will error out.Works now
             'customfield_11626':'Change Request', #this is impround field
-            'description':context,
+            'description':'*Detailed Scope:*\n'+scope+'\n\n*Context of Need:*\n'+context,
             'issuetype': {'name': 'Epic'},
             'customfield_11618':{'value':'Custom or Unknown'},
-            'customfield_11630': {'value': 'Unknown'},  # customer contract id. manadatory For all new tickets.
+            'customfield_11630': {'value': finid},  # customer contract id. manadatory For all new tickets.
             'customfield_10301': crname , # Epic name. Mandatory field. For all new tickets.
             'assignee': {'name': watcher},
+            'duedate': duedate
             #'timeoriginalestimate': '5',
             #'timetracking':{'originalEstimate': '4d','remainingEstimate':'1d'}, #Set estimated time to completion from CR form. Not working now.
             #'customfield_10005': 'value'[0][2].append('Next Step Outside Connector') # {'name':'Next Step Outside Connector'} #do this later. this is the sprint field.
 
             #jira.add_watcher('AAI-75148', 'siddhesh.narkar') #this is the script to add watcher. add issue.id instead of AAI and the reporter username from sql
         }
+
+        print ('Here are the variables we need: scope {}, finid {}, due date {} '.format(scope,finid,duedate))
+        print ('\n',issue_d)
+
 
         pprint.pprint(issue_d)
         # logging.debug(pprint.pprint(issue_d))
@@ -125,9 +141,8 @@ else:
 
 
         #Update the dashboard with the ticket number. Important. Works
-        cursor.execute('''update ARC_OrderFormValues
-        set jira_ticket=? where ID=?''', str(issue), id1)
-        cnxn.commit()
+        # cursor.execute("update ARC_OrderFormValues set jira_ticket=? where ID=?", str(issue), id1)
+        # cnxn.commit()
 
         # logging.debug(issue.id)
         logging.debug('JIRA Ticket number:'+str(issue))
@@ -145,3 +160,4 @@ else:
         jira.add_watcher(issue.id, watcher)
 
         jira.add_comment(issue.id, 'Your CR has been Created')
+
