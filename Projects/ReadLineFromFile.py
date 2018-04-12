95a0798b186ec32021aaf1a9e6c9a2279a09ab9a
dineshjayapathy <dinesh.jayapathy@arcadiasolutions.com>
2018-04-12 17:23:52 -0400

    Refinement

--- Projects/ReadLineFromFile.py
+++
@@ -1,9 +0,0 @@
-count = 0
-with open(r"C:\DJ\MISC\data\20170815123912_CAREMOUNT_IPA_CLAIMS_PHARMACY_EXTRACT_ADHOC") as f:#change your file path here
-    for i in f:
-        count+=1
-        # print i
-        if count == 10000:#enter your line number here
-            print count, i
-            # count += 1
-
